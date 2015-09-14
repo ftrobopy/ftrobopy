6044a23a077e93d7529dd20ac23cd161330f7d89
Torsten Stuehn <stuehn@lt65.local>
2015-09-14 22:57:15 +0200

    add example, put some sense in README (in german)

---
+++ SimpleDemoExplorer.py
@@ -0,0 +1,22 @@
+import sys
+import ftrobopy
+
+txt=ftrobopy.ftrobopy('192.168.8.2', 65000)
+
+Motor_rechts = txt.motor(1)
+Motor_links  = txt.motor(2)
+Ultraschall  = txt.ultrasonic(8)
+Motor_rechts.setSpeed(512)
+Motor_links.setSpeed(512)
+Motor_rechts.setDistance(1000, syncto=Motor_links)
+Motor_links.setDistance(1000, syncto=Motor_rechts)
+
+while not Motor_rechts.finished(): 
+  d=Ultraschall.distance()
+  print d
+  if d<10:
+    Motor_rechts.stop()
+    Motor_links.stop()
+    if txt.sound_finished():
+      txt.play_sound(3,1)
+
