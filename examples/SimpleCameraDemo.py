505c8802f4d5883b0e651e2dedbd8970984ddc14
Torsten Stuehn <stuehn@mailbox.org>
2017-01-29 14:42:12 +0100

    improve SimpleCameraDemo.py

--- examples/SimpleCameraDemo.py
+++ examples/SimpleCameraDemo.py
@@ -1,16 +1,18 @@
-import sys, time
+import time
 import ftrobopy
 
 # Einlesen und Abspeichern eines TXT Camera Bildes (jpeg)
 
-txt=ftrobopy.ftrobopy('192.168.7.2', 65000)
+txt=ftrobopy.ftrobopy('auto')
+time.sleep(2)
 txt.startCameraOnline()
 time.sleep(2.5)
 pic = None
 while pic == None:
   pic = txt.getCameraFrame()
+  if pic != None:
+    if len(pic) == 0:
+      pic = None
 
-f=open('TXTCamPic.jpg','w')
-f.write(''.join(pic))
-f.close()
-
+with open('TXTCamPic.jpg', 'w') as f:
+  f.write(''.join(pic))
