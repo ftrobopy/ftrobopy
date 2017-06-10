7585225ae64ded12ea4b55ba3bf964c88f439395
Torsten Stuehn <stuehn@mailbox.org>
2017-06-10 10:02:34 +0200

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup, Extension
 
 setup(name='ftrobopy',
       description='Python Interface for Fischertechnik ROBOTICS TXT Controller',
-      version='1.67',
+      version='1.75',
       author='Torsten Stuehn',
       author_email='Torsten Stuehn',
       url='https://github.com/ftrobopy/ftrobopy',
+      download_url='https://github.com/ftrobopy/ftrobopy/archive/1.75.tar.gz',
       license='MIT',
       py_modules=['ftrobopy'],
       ext_modules=[Extension('ftrobopytools',
