dc556e078cea8e8740d9e05ca36c59ffba1598ec
Torsten Stuehn <stuehn@mailbox.org>
2025-08-08 11:51:03 +0200

    created release v2.0.2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(name='ftrobopy',
       description='Python Interface for Fischertechnik ROBOTICS TXT Controller',
-      version='2.0.0',
+      version='2.0.2',
       author='Torsten Stuehn',
       author_email='stuehn@mailbox.org',
       url='https://github.com/ftrobopy/ftrobopy',
-      download_url='https://github.com/ftrobopy/ftrobopy/archive/2.0.0.tar.gz',
+      download_url='https://github.com/ftrobopy/ftrobopy/archive/refs/tags/v2.0.2.tar.gz',
       license='MIT',
       py_modules=['ftrobopy']
       )
